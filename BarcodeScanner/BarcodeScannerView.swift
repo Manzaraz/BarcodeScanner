f31eac7d961fba5a271611ba0e67000d28d845bb
Christian Manzaraz <magnusmanz@gmail.com>
2024-07-06 17:47:44 -0300

    Camera Setup

---
+++ BarcodeScanner/BarcodeScannerView.swift
@@ -0,0 +1,38 @@
+//
+//  BarcodeScannerView.swift
+//  BarcodeScanner
+//
+//  Created by Christian Manzaraz on 06/07/2024.
+//
+
+import SwiftUI
+
+struct BarcodeScannerView: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                Rectangle()
+                    .frame(maxWidth: .infinity, maxHeight: 300)
+                
+                Spacer()
+                    .frame(height: 40)
+                
+                Label("Scanner Barcode: ", systemImage: "barcode.viewfinder")
+                    .font(.title)
+                
+                Text("Not Yet Scanned")
+                    .bold()
+                    .font(.largeTitle)
+                    .foregroundStyle(.green)
+                    .padding()
+                
+            }
+            .navigationTitle("Barcode Scanner")
+        }
+        
+    }
+}
+
+#Preview {
+    BarcodeScannerView()
+}
