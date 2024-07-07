7b2b00d1a0aff18108fa798264aba7f46f9811b2
Christian Manzaraz <magnusmanz@gmail.com>
2024-07-06 21:06:16 -0300

    More Error Handling

--- BarcodeScanner/ScannerVC.swift
+++ BarcodeScanner/ScannerVC.swift
@@ -8,11 +8,10 @@
 import UIKit
 import AVFoundation
 
-enum CameraError: String {
-    case invalidDeviceInput = "Something went wrong with the camera. We are unable to capture the input."
-    case invalidScannedValue = "The value scanned is not valid. This app scans EAN-8 and EAN-13 barcode types."
+enum CameraError {
+    case invalidDeviceInput
+    case invalidScannedValue
 }
-
 
 //protocol ScannerVCDelegate: class {
 protocol ScannerVCDelegate: AnyObject {
@@ -33,6 +32,22 @@ class ScannerVC: UIViewController {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupCaptureSession()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        guard let previewLayer = previewLayer else {
+            scannerDelegate?.didSurface(error: .invalidDeviceInput)
+            return
+        }
+        
+        previewLayer.frame = view.layer.bounds
     }
     
     private func setupCaptureSession() {
@@ -92,6 +107,7 @@ extension ScannerVC: AVCaptureMetadataOutputObjectsDelegate {
             return
         }
         
+//        captureSession.stopRunning()
         scannerDelegate?.didFind(barcode: barcode)
     }
 }
